7e0021203595e7dea6db0a22387f645bc0f1f86b
AndrewFerguson1 <155381264+AndrewFerguson1@users.noreply.github.com>
2025-08-21 14:25:32 +0000

    blahhhh

---
+++ main.py
@@ -0,0 +1,21 @@
+class person ():
+    def __init__(self): # constructor
+        self.name = 'Innes' # String
+        self.age = 17 # Integer
+        self.colour = 'White' # String
+
+def get_name(self):
+    return self.name
+
+def get_age(self):
+    return self.age
+
+def get_colour(self):
+    return colour.self
+
+def setName(self, newName):
+    self.name = newName
+
+# outside of class definition
+people = [person() for x in range(10)]
+print(people[0].get_name())
